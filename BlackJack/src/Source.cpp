25f52fa8eee10bb350434de8afdeb44326c0fe6d
ExPikaPaka <112851715+ExPikaPaka@users.noreply.github.com>
2023-07-06 20:30:01 +0300

    Added FPS display on window title & command-line argument parrser, to override fps via '--FPS 60' parameter

--- BlackJack/src/Source.cpp
+++ BlackJack/src/Source.cpp
@@ -3,33 +3,60 @@
 Game* game = nullptr;
 
 #include "BlackJack/BlackJack.h"
+#include <iostream>
+#include <string>
+
 int main(int argc, char* argv[]) {
-	system("chcp 65001&cls");
+    system("chcp 65001&cls");
 
-	game = new Game();
-	game->init("BlackJack", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1366, 768, 0);
+    int FPS = 60; // Default FPS value
 
-	const int FPS = 60;
-	const int frameDelay = 1000 / FPS;
+    // Parse command-line arguments
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
 
-	Uint32 frameStart, frameTime;
+        if (arg == "--FPS" && i + 1 < argc) {
+            FPS = std::stoi(argv[i + 1]); // Get the FPS value from the next argument
+        }
+    }
 
-	while (game->running()) {
-		frameStart = SDL_GetTicks();
+    game = new Game();
+    game->init("BlackJack", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1366, 768, 0);
 
-		game->handleEvents();
-		game->update();
-		game->render();
+    const int frameDelay = 1000 / FPS;
 
-		frameTime = SDL_GetTicks() - frameStart;
-		if (frameDelay > frameTime) {
-			SDL_Delay(frameDelay - frameTime);
-		}
-	}
+    Uint32 frameStart, frameTime;
+    Uint32 frameCount = 0;
+    Uint32 fpsTimer = SDL_GetTicks(); // Initialize the timer with current ticks
 
-	game->clean();
+    while (game->running()) {
+        frameStart = SDL_GetTicks();
 
-	return 0;
+        game->handleEvents();
+        game->update();
+        game->render();
+
+        frameCount++;
+
+        // Calculate FPS every second
+        if (SDL_GetTicks() - fpsTimer >= 1000) {
+            std::string windowTitle = "BlackJack - FPS: " + std::to_string(frameCount);
+            SDL_SetWindowTitle(game->getWindow(), windowTitle.c_str());
+
+            frameCount = 0;
+            fpsTimer = SDL_GetTicks(); // Reset the timer
+        }
+
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameDelay > frameTime) {
+            SDL_Delay(frameDelay - frameTime);
+        }
+    }
+
+    game->clean();
+
+    return 0;
 }
+
 
 
