f9dabdbbe5d5bdad1bbaf446ff20f35101d5132b
ExPikaPaka <112851715+ExPikaPaka@users.noreply.github.com>
2023-07-06 20:57:58 +0300

    Refactoring code for improved readability

--- BlackJack/src/TextureLoader/TextureLoader.cpp
+++ BlackJack/src/TextureLoader/TextureLoader.cpp
@@ -1,7 +1,7 @@
 #include "TextureLoader.h"
 
-SDL_Texture* TextureLoader::loadTexture(const char* fileName, SDL_Renderer* renderer) {
-    SDL_Surface* tempSurface = IMG_Load(fileName);
+SDL_Texture* TextureLoader::loadTexture(const char* filePath, SDL_Renderer* renderer) {
+    SDL_Surface* tempSurface = IMG_Load(filePath);
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, tempSurface);
     SDL_FreeSurface(tempSurface);
 
--- BlackJack/src/TextureLoader/TextureLoader.h
+++ BlackJack/src/TextureLoader/TextureLoader.h
@@ -3,9 +3,14 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+// Texture loader class
 class TextureLoader {
 
 public:
-	static SDL_Texture* loadTexture(const char* fileName, SDL_Renderer* renderer);
+	// Loads texture from given filePath.
+	// I
+	// You can use TextureLoader::loadTexture(...) function without class object
+	// due to the static function
+	static SDL_Texture* loadTexture(const char* filePath, SDL_Renderer* renderer);
 };
 
